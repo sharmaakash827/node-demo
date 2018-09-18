a4af740d50ce1691b97160e15900467c44c301ed
Akash Sharma <sharma.akash@tftus.com>
2018-09-18 13:34:48 +0530

    Nightwatch Assignment 1

---
+++ Nightwatch-Demo/tests/dhanuka.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'Download Report Test' : function (browser) {
+    browser
+      .url('http://203.110.85.168:8013')
+      .waitForElementVisible('#ecn_no', 5000)
+      .setValue('#ecn_no', '1000')
+      .setValue('#password', 'dhanuka@123')
+      .click('input[type=submit]')
+      .waitForElementVisible('h3', 5000)
+      //.pause(5000)
+      .assert.containsText('h3', 'Home Page')
+      .useXpath()
+      .click('//ul[@class="nav"]/li[5]/a')
+      .click('//ul[@class="nav"]/li[5]/ul/li[1]/a')
+      .useCss()
+      .waitForElementVisible('button[type=submit]',5000)
+      .setValue('input[name=StartDate]','2018-09-13')
+      .setValue('input[name=EndDate]','2018-09-13')
+      .useXpath()
+      //.click('//div[@class="row ng-star-inserted"]//button[@class="ui-autocomplete-dropdown ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ng-star-inserted"]/span[@class="ui-button-text ui-clickable"]')
+      .click('//span[@class="ui-button-text ui-clickable"]')
+      .waitForElementVisible('//li[@class="ui-autocomplete-list-item ui-corner-all"][1]/span',5000)
+      .click('//li[@class="ui-autocomplete-list-item ui-corner-all"][1]/span')
+      .pause(2000)
+      .useCss()
+      .click('button[type=submit]')
+      .pause(10000)
+      .end();
+  }
+};
---
+++ Nightwatch-Demo/tests/search.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'Demo test Google' : function (browser) {
+    browser
+      .url('https://www.google.co.in')
+      .waitForElementVisible('.gsfi', 5000)
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('input[type=submit]', 1000)
+      .click('input[type=submit]')
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
+      .end();
+  }
+};
