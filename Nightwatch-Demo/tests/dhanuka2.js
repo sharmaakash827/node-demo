94a0047c3817065cbec5cb59089e1163065dca56
Akash Sharma <sharma.akash@tftus.com>
2018-09-19 19:22:09 +0530

    test using custom-command

---
+++ Nightwatch-Demo/tests/dhanuka2.js
@@ -0,0 +1,27 @@
+module.exports = {
+	'Download Report Test' : function(something){
+		something
+			.url('http://203.110.85.168:8013')
+
+			// This is the custom command used here.
+			.login('1000','dhanuka@123') 
+			
+			.useXpath()
+	    .click('//ul[@class="nav"]/li[5]/a')
+	    .click('//ul[@class="nav"]/li[5]/ul/li[1]/a')
+	    .useCss()
+	    .waitForElementVisible('button[type=submit]',5000)
+	    .setValue('input[name=StartDate]','2018-09-13')
+	    .setValue('input[name=EndDate]','2018-09-13')
+	    .useXpath()
+	    .click('//span[@class="ui-button-text ui-clickable"]')
+	    .waitForElementVisible('//li[@class="ui-autocomplete-list-item ui-corner-all"][1]/span',5000)
+	    .click('//li[@class="ui-autocomplete-list-item ui-corner-all"][1]/span')
+	    .pause(2000)
+	    .useCss()
+	    .click('button[type=submit]')
+	    .pause(10000)
+	    .end();
+
+	}
+}
