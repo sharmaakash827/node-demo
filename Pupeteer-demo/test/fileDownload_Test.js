df8ec9aac94c955e2b68d5129a45ae4e188c98f4
Akash Sharma <sharma.akash@tftus.com>
2019-11-15 20:00:38 +0530

    fixed

--- Pupeteer-demo/test/fileDownload_Test.js
+++ Pupeteer-demo/test/fileDownload_Test.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 // var Base = require('../helpers/base');
 var login = require('../helpers/pages/login');
 const fs = require('file-system');
-const downloadPath = __dirname+'/myFiles';
+const filePath = __dirname+'/myFiles';
 
 describe.only('Download File Test', () => {
 
@@ -13,16 +13,14 @@ describe.only('Download File Test', () => {
         await page.click('a[href="/navs/data-dump"]');
         await page.click('a[routerlink="/data-dump/fuel-rate');
         await page.waitForSelector('button[class="btn btn-success"]');
-        await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: downloadPath});
+        await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: filePath});
         await page.click('button[class="btn btn-success"]');
     
         await page.waitFor(5000);   
 
         await fs.readdir(downloadPath, async function(err, items) {
-        // console.log(items);
-    
             await expect(items[0]).to.equal('fuel-rate.xlsx');
-            await fs.rmdirSync(downloadPath);
+            await fs.rmdirSync(filePath);
         });
     });
 })
